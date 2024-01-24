6ca11c8d438d4c54ed39781db9460480760bc7b9
jjandrew <jja211@exeter.ac.uk>
2024-01-24 12:50:23 +0000

    Added tests for new functions

--- tests/testUtils.py
+++ tests/testUtils.py
@@ -4,7 +4,7 @@ Tests for the repeated functions throughout the project
 
 import unittest
 from src.classes.hub import Hub
-from src.utils import calc_distance
+from src.utils import calc_distance, get_closest_hub, reduce_model
 
 
 class TestModelCreation(unittest.TestCase):
@@ -16,9 +16,9 @@ class TestModelCreation(unittest.TestCase):
     def setUp(self):
         """Setup for the tests (to create the model)"""
         # Create 3 hubs
-        hub0 = Hub(name=0, s=0, long=0, lat=0)
+        hub0 = Hub(name=0, s=-5, long=0, lat=0)
         hub1 = Hub(name=1, s=0, long=1, lat=2)
-        hub2 = Hub(name=2, s=0, long=2, lat=0)
+        hub2 = Hub(name=2, s=10, long=2, lat=0)
 
         # Place in an array
         self.model = [hub0, hub1, hub2]
@@ -42,3 +42,29 @@ class TestModelCreation(unittest.TestCase):
         dist = calc_distance(path=solution, hubs=self.model)
 
         self.assertEqual(11, dist)
+
+    def test_get_closest_hub(self):
+        """
+        Tests it is possible to get a closest hub
+        """
+        # Use hub 0 from the model
+        hub = self.model[0]
+        # Find its closest hub
+        closest_hub = get_closest_hub(hub=hub, model=self.model)
+
+        # check this is hub 1
+        self.assertEqual(closest_hub, self.model[2])
+
+    def test_reduce_model(self):
+        """
+        Tests the model can be successfully reduced
+        """
+        model_reduction_journeys = reduce_model(
+            model=self.model, max_journey_size=3)
+
+        expected_journeys = [
+            {'from': 2, 'to': 0, 's': 3},
+            {'from': 2, 'to': 0, 's': 2}
+        ]
+
+        self.assertEqual(model_reduction_journeys, expected_journeys)
