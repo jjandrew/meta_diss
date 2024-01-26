365c5479a204dc547b2ac8b2f90ad7c6351aa394
jjandrew <jja211@exeter.ac.uk>
2024-01-26 16:54:34 +0000

    Added a function to calculate all possible next steps

--- src/main.py
+++ src/main.py
@@ -1,6 +1,17 @@
 from model.model import create_model
 from visualise import plot_network
+from classes.hub import Hub
+from searches.brute_force.brute import next_steps
 
 if __name__ == "__main__":
-    model = create_model(n=10, alpha=10)
-    plot_network(model=model)
+    starting_hub = Hub(name=0, s=2, long=0, lat=0)
+
+    def_hub_1 = Hub(name=1, s=-1, long=0, lat=0)
+    def_hub_2 = Hub(name=2, s=-2, long=0, lat=0)
+    def_hub_3 = Hub(name=3, s=-5, long=0, lat=0)
+
+    def_hubs = [def_hub_1, def_hub_2, def_hub_3]
+
+    next_js = next_steps(starting_hub=starting_hub,
+                         deficit_hubs=def_hubs, max_journey_size=3)
+    print(next_js)
--- src/searches/brute_force/brute.py
+++ src/searches/brute_force/brute.py
@@ -3,12 +3,42 @@ A brute force approach to solving the problem
 """
 from typing import List, Dict
 from classes.hub import Hub
-from utils import reduce_model, calc_distance, improve_solution
+from utils import reduce_model, calc_distance, improve_solution, is_resolved
 from math import inf
+import copy
 
 
-# Now for testing
-from model.model import create_model
+def next_steps(starting_hub: Hub, deficit_hubs: List[Hub], max_journey_size: int) -> List[Dict[str, int]]:
+    """
+    Find all possible routes from a surplus hub to deficit hubs
+
+    params:
+        starting_hub - The surplus hub to look at next journeys from
+        deficit_hubs - A list of all of the deficit hubs
+        max_journey_size - The maximum quanitity that can be moved per journey
+
+    Returns
+        List of dictionaries of {from, to, s} of each journey
+    """
+    # A list of the journeys
+    journeys = []
+
+    # Go through each of the deficit hubs
+    for def_hub in deficit_hubs:
+        # Check deficit hub isn't resolved
+        if def_hub.get_s() == 0:  # If it is
+            continue  # Ignore it
+
+        # Pick a quantity to move between 1 and minimum of max_journey_size, surplus_hub s and deficit hub s
+        for s in range(1, min(max_journey_size, starting_hub.get_s(), abs(def_hub.get_s())) + 1):
+            # Add the journey to next possible journeys
+            journeys.append({
+                'from': starting_hub.get_name(),
+                'to': def_hub.get_name(),
+                's': s
+            })
+
+    return journeys
 
 
 def brute(model: List[Hub], max_journey_size: int) -> List[Dict[str, int]]:
@@ -25,7 +55,22 @@ def brute(model: List[Hub], max_journey_size: int) -> List[Dict[str, int]]:
     # Reduce the model to get a list of guaranteed shortest journeys
     journeys = reduce_model(model=model, max_journey_size=max_journey_size)
 
-    # generate all possible journeys
+    # Now perform brute force on the rest of the model
+
+    # Split hubs into surplus and deficit hubs
+    surplus_hubs = [hub for hub in model if hub.get_s() > 0]
+    deficit_hubs = [hub for hub in model if hub.get_s() < 0]
+
+    paths = []
+    # For each surplus hub
+    for starting_hub in surplus_hubs:
+        # Get all possible jouneys from the starting hub
+        # To do this look at all of the deficit hubs
+        print()
+
+    hubs_to_resolve: List[Hub] = copy.copy(model)
+
+    #########################################################################################################
 
     # Initialize variables to track the best solution
     best_sequence = None
@@ -44,14 +89,3 @@ def brute(model: List[Hub], max_journey_size: int) -> List[Dict[str, int]]:
             best_sequence = improved_path
 
     return best_sequence
-
-
-if __name__ == "__main__":
-    model = create_model(n=4, alpha=2, max_def=-10, max_sur=10)
-    print("Starting model")
-    for hub in model:
-        print(hub)
-
-    print()
-    print("Brute force")
-    brute(model=model, max_journey_size=3)
