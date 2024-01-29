0f125ea8b61b49b48e28ce69f924120ec4df704a
jjandrew <jja211@exeter.ac.uk>
2024-01-29 17:53:04 +0000

    Added pheromone functions

---
+++ src/searches/aco/pheromone.py
@@ -0,0 +1,34 @@
+"""
+Methods for the updating of a pheromone
+"""
+from typing import List, Dict
+
+
+def update_pheromone(p: List[List[float]], paths: List[List[Dict[str, int]]], costs: List[int], e: float, Q: int):
+    """
+    Updates the pheromone matrix of a population of ants
+
+    params
+        p - Pheromone matrix
+        paths - list of paths in the population
+        costs - list of the corresponding path costs
+        e - evaporation rate (% of pheromone removed with each population)
+        Q - A scaling constant for pheromone
+    """
+    # Evaporate edges by e%
+    for v in range(len(p)):
+        for n in range(len(p[v])):
+            p[v][n] *= 1-e
+
+    # Deposit pheromone
+    for i in range(len(paths)):
+        # Obtain the cost and path for each passed in
+        path = paths[i]
+        cost = costs[i]
+        # Add Q / cost to each of the edges used in the path
+        to_add = Q / cost
+
+        # Add the pheromone for each journey in the
+        for journey in path:
+            # Add to pheromone matrix for p[from][to]
+            p[journey['from']][journey['to']] += to_add
