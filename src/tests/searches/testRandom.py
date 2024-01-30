4ee21f00434a651a1e34f2afc7c2a72e33e3ee88
jjandrew <jja211@exeter.ac.uk>
2024-01-30 13:48:42 +0000

    Added search to make sure all hubs included in random solution

--- src/tests/searches/testRandom.py
+++ src/tests/searches/testRandom.py
@@ -12,7 +12,8 @@ class TestRandomSolutionClass(unittest.TestCase):
     """
     Test clas for the random search method
     """
-    model = create_model(n=10000, alpha=2)
+    n = 1000
+    model = create_model(n=n, alpha=2)
 
     def test_random_search(self):
         """Checks that the function returns a valid solution"""
@@ -21,11 +22,19 @@ class TestRandomSolutionClass(unittest.TestCase):
         for hub in self.model:
             model_abs_s += abs(hub.get_s())
 
+        # Obtain a solution
         solution = random_search(model=self.model, max_journey_size=20)
 
+        # Calculate total S and unique hubs
         solution_s = 0
+        hubs = set()
         for s in solution:
             solution_s += s['s']
+            hubs.add(s['to'])
+            hubs.add(s['from'])
 
         # Assert absolute sum of solution s = 2 * sum of absolute of original s of model
         self.assertEqual(model_abs_s, solution_s * 2)
+
+        # Check n unique hubs all used in solution
+        self.assertEqual(self.n, len(hubs))
