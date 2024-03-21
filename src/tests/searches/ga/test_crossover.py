c9df3286f2e657250f1427a6d84fc981a3dea815
jjandrew <jja211@exeter.ac.uk>
2024-03-21 16:05:44 +0000

    Resolved crossover functions

--- src/tests/searches/ga/test_crossover.py
+++ src/tests/searches/ga/test_crossover.py
@@ -8,23 +8,23 @@ from searches.ga.population import encode_solution, decode_solution
 from utils import is_complete
 
 
-class TestUniformCrossover(unittest.TestCase):
+class TestCrossoverClass(unittest.TestCase):
     """
-    Test class for the uniform crossover function
+    Test class for the context-aware crossover function
     """
 
     def test_crossover_works_when_parents_identical(self):
         """
         Tests the crossover method returns two children identical to the parents
         """
-        hub_0 = Depot(name=0, s=28, long=0, lat=0)
-        hub_1 = Depot(name=1, s=-4, long=0, lat=0)
-        hub_2 = Depot(name=2, s=-84, long=0, lat=0)
-        hub_3 = Depot(name=3, s=65, long=0, lat=0)
-        hub_4 = Depot(name=4, s=-16, long=0, lat=0)
-        hub_5 = Depot(name=5, s=11, long=0, lat=0)
+        dep_0 = Depot(name=0, s=28, long=0, lat=0)
+        dep_1 = Depot(name=1, s=-4, long=0, lat=0)
+        dep_2 = Depot(name=2, s=-84, long=0, lat=0)
+        dep_3 = Depot(name=3, s=65, long=0, lat=0)
+        dep_4 = Depot(name=4, s=-16, long=0, lat=0)
+        dep_5 = Depot(name=5, s=11, long=0, lat=0)
 
-        model = {0: hub_0, 1: hub_1, 2: hub_2, 3: hub_3, 4: hub_4, 5: hub_5}
+        model = {0: dep_0, 1: dep_1, 2: dep_2, 3: dep_3, 4: dep_4, 5: dep_5}
 
         # Generate two unique solutions to the problem
         parent1 = [{'from': 3, 'to': 4, 's': 16}, {'from': 0, 'to': 2, 's': 20}, {'from': 0, 'to': 2, 's': 8},
@@ -44,7 +44,7 @@ class TestUniformCrossover(unittest.TestCase):
 
         # Perform the aware crossover
         res_child_1, res_child_2 = aware_crossover(
-            parent_1=parent1, parent_2=parent2, model=model, max_journey_size=20)
+            parent_1=parent1, parent_2=parent2, model=model, max_journey_size=20, crossover_rate=1)
 
         # Check that the children are valid solutions
         res_child_1_decoded = decode_solution(path=res_child_1)
@@ -78,3 +78,39 @@ class TestUniformCrossover(unittest.TestCase):
 
         self.assertGreater(count_in_1, 0)
         self.assertGreater(count_in_2, 0)
+
+    def test_no_crossover_performed_when_rate_of_0(self):
+        """
+        Tests that the parents are returned as children when a crossover rate of 0 is used
+        """
+        dep_0 = Depot(name=0, s=28, long=0, lat=0)
+        dep_1 = Depot(name=1, s=-4, long=0, lat=0)
+        dep_2 = Depot(name=2, s=-84, long=0, lat=0)
+        dep_3 = Depot(name=3, s=65, long=0, lat=0)
+        dep_4 = Depot(name=4, s=-16, long=0, lat=0)
+        dep_5 = Depot(name=5, s=11, long=0, lat=0)
+
+        model = {0: dep_0, 1: dep_1, 2: dep_2, 3: dep_3, 4: dep_4, 5: dep_5}
+
+        # Generate two unique solutions to the problem
+        parent1 = [{'from': 3, 'to': 4, 's': 16}, {'from': 0, 'to': 2, 's': 20}, {'from': 0, 'to': 2, 's': 8},
+                   {'from': 3, 'to': 2, 's': 20}, {'from': 3, 'to': 1,
+                                                   's': 4}, {'from': 3, 'to': 2, 's': 20},
+                   {'from': 5, 'to': 2, 's': 11}, {'from': 3, 'to': 2, 's': 5}]
+
+        encoded_parent_1 = encode_solution(path=parent1)
+
+        parent2 = [{'from': 0, 'to': 2, 's': 20}, {'from': 3, 'to': 2, 's': 20}, {'from': 0, 'to': 2, 's': 8}, {'from': 5, 'to': 4, 's': 11}, {
+            'from': 3, 'to': 2, 's': 20}, {'from': 3, 'to': 1, 's': 4}, {'from': 3, 'to': 4, 's': 5}, {'from': 3, 'to': 2, 's': 16}]
+
+        encoded_parent_2 = encode_solution(path=parent2)
+
+        parent1 = encode_solution(path=parent1)
+        parent2 = encode_solution(path=parent2)
+
+        # Perform the aware crossover
+        res_child_1, res_child_2 = aware_crossover(
+            parent_1=parent1, parent_2=parent2, model=model, max_journey_size=20, crossover_rate=0)
+
+        self.assertEqual(res_child_1, encoded_parent_1)
+        self.assertEqual(res_child_2, encoded_parent_2)
