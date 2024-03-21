7b1858f758950258351a038ba8edfb285296fe6c
jjandrew <jja211@exeter.ac.uk>
2024-03-21 14:47:19 +0000

    Refactored AS.py

--- src/searches/aco/AS.py
+++ src/searches/aco/AS.py
@@ -1,22 +1,22 @@
 """
 Executes the main body of the AS algorithm
 """
-from typing import List, Dict
-from model.hub import Hub
 from math import inf
+from typing import List, Dict
+from model.depot import Depot
 from searches.aco.pathGeneration import generate_path
 from searches.aco.pheromone import update_pheromone
 from utils import fitness
 
 
-def AS(model: Dict[int, Hub], m: int, e: float, Q: int, d: List[List[float]],
+def AS(model: Dict[int, Depot], m: int, e: float, Q: int, d: List[List[float]],
        p: List[List[float]], h: List[List[float]], n: int, max_journey_size: int,
        alpha=1, beta=2) -> int:
     """
-    Performs the AS algorithm on the data
+    Performs the AS algorithm on the TNRP
 
     params:
-    model - The model as dictionary of hub_name: Hub object
+    model - The model as dictionary of depot_name: Depot object
     m - population size
     e - evaporation rate
     Q - constant for fitness normalisation
@@ -24,83 +24,62 @@ def AS(model: Dict[int, Hub], m: int, e: float, Q: int, d: List[List[float]],
     p - pheromone matrix
     n - Termination condition - number of fitness evals before termination
     max_journey_size - The maximum size of each journey
-    alpha - The exponent used to scale the pheromone matrix
-    beta - The exponent used to scale the heuristic matrix
+    alpha - The exponent used to scale the pheromone matrix in transition probabilities (default=1)
+    beta - The exponent used to scale the heuristic matrix in transition probabilities (default=2)
 
     returns:
-    min cost route
+        List of fitnesses at each evaluation
     """
-    # TODO maybe add in a heuristic matrix using s / total_s
-    # TODO maybe need to add a 3rd dimension into p of s to be moved
+    # Split depots into surplus and deficit depots
+    surplus_deps = {dep: model[dep] for dep in model if model[dep].get_s() > 0}
+    deficit_deps = {dep: model[dep] for dep in model if model[dep].get_s() < 0}
 
-    # Split hubs into surplus and deficit hubs
-    surplus_hubs = {hub: model[hub] for hub in model if model[hub].get_s() > 0}
-    deficit_hubs = {hub: model[hub] for hub in model if model[hub].get_s() < 0}
-
-    # Set min fitness to infinity and path to empty as no better solution found
-    min_fitness = inf
-    min_path = []
-
-    # Store fitness as progression
+    # Empty array to store results of all fitness evals
     all_fitnesses = []
 
-    # Num fitness evals for termination criterion
+    # Store the best path so far
+    best_path = []
+    best_fitness = inf
+
+    # Variable for number of fitness evaluations so far
     fitness_evals = 0
 
-    # Store start fitness to check for convergence
-    start_fitness = -1
-    start_path = []
-
-    # print(p)
     # Repeat until termination criterion is met
     while fitness_evals < n:
+        # Store the paths and fitnesses of the population
         paths = []
         fitnesses = []
 
         # Create a population of m ants:
         for _ in range(m):
 
-            # Check if fitness condition broken
+            # Check if the termination condition is broken part-way through creation of population
             if fitness_evals == n:
+                # If so end creation of population
                 break
 
-            # Generate a path
+            # Generate a path using heuristic and pheromone information
             path = generate_path(
-                sur_hubs=surplus_hubs, def_hubs=deficit_hubs, d=d, h=h, p=p, max_journey_size=max_journey_size, alpha=alpha, beta=beta)
+                sur_deps=surplus_deps, def_deps=deficit_deps, d=d, h=h, p=p,
+                max_journey_size=max_journey_size, alpha=alpha, beta=beta)
 
             # Calculate the fitness of the solution
             ant_fitness = fitness(path=path, model=model)
-
+            # Increment the number of fitness evaluations
+            fitness_evals += 1
+            # Add the fitness evaluation to the fitnesses list
             all_fitnesses.append(ant_fitness)
 
-            fitness_evals += 1
-
-            if ant_fitness < min_fitness:
-                min_fitness = ant_fitness
-                min_path = path
+            # If the ant has the best so far fitness then store its path
+            if ant_fitness < best_fitness:
+                best_path = path
+                best_fitness = ant_fitness
 
             # Add paths and fitnesses to population paths and fitnesses
             paths.append(path)
-            # print()
-            # print(path)
             fitnesses.append(ant_fitness)
 
         # Update the pheromone using the population
-
         update_pheromone(p=p, paths=paths, fitnesses=fitnesses, e=e, Q=Q)
 
-        # print()
-        # print()
-        # print(p)
-
-    # for row in p:
-    #     print(row)
-
-    # print()
-    # start_path.sort(key=lambda j: (j['from'], j['to'], -j['s']))
-    # print(start_path)
-    # print()
-    # min_path.sort(key=lambda j: (j['from'], j['to'], -j['s']))
-    # print(min_path)
-
-    return all_fitnesses
+    return all_fitnesses, best_path
---
+++ src/tests/searches/aco/test_AS.py
@@ -0,0 +1,41 @@
+"""
+Tests for the main ACO algorithm
+"""
+import unittest
+from typing import List, Dict
+from searches.aco.create_matrices import create_dist_matrix, create_pher_matrix, create_heur_matrix
+from searches.aco.AS import AS
+from model.depot import Depot
+from model.tnrp_model import create_model
+
+
+class TestASClass(unittest.TestCase):
+    """
+    Class for the testing of the AS algorithm
+    """
+    model = {}
+
+    def test_AS(self):
+        """
+        Tests the AS algorithm uses the correct representation method for paths
+        """
+        model = create_model(n=50, alpha=2)
+
+        # Generate the distance and pheromone matrices
+        d = create_dist_matrix(model=model)
+        p = create_pher_matrix(model=model, dist_matrix=d)
+        h = create_heur_matrix(dist_matrix=d)
+
+        max_journey_size = 5
+
+        # Perform the AS algorithm
+        _, path = AS(
+            model=model, m=1, e=0.5, Q=10, d=d, p=p, h=h, n=100, max_journey_size=max_journey_size)
+
+        # Check that the best path is of the correct type
+        self.assertIsInstance(path, List)
+        for journey in path:
+            self.assertIsInstance(journey, Dict)
+            for key, value in journey.items():
+                self.assertIsInstance(key, str)
+                self.assertIsInstance(value, int)
