9580af2dee57ea89063f5c21aef2ee3b6d4618e3
jjandrew <jja211@exeter.ac.uk>
2024-03-21 15:02:24 +0000

    Resolved aco matrix creation

--- src/searches/aco/create_matrices.py
+++ src/searches/aco/create_matrices.py
@@ -1,17 +1,17 @@
 """
 Creates distance and pheromone matrices for the problem
 """
-from typing import List, Dict
-from model.hub import Hub
 import random
+from typing import List, Dict
+from model.depot import Depot
 
 
-def create_dist_matrix(model: Dict[int, Hub]) -> List[List[float]]:
+def create_dist_matrix(model: Dict[int, Depot]) -> List[List[float]]:
     """
     Creates the distance matrix from the model passed in
 
     Params
-        model - A list of hubs that is passed in
+        model - The TNRP model as a dictionary of depot name: DepotObj
 
     Returns:
         List where dist_matrix[i][j] is the distance on edge i-j
@@ -21,13 +21,13 @@ def create_dist_matrix(model: Dict[int, Hub]) -> List[List[float]]:
     dist_matrix = [[0] * len(model) for _ in range(len(model))]
 
     # Add the connections into the dist_matrix
-    for hub_name in model:
-        hub = model[hub_name]
-        # Get the name and connections of the hub
-        name = hub.get_name()
-        connections = hub.get_connections()
+    for dep_name in model:
+        dep = model[dep_name]
+        # Get the name and connections of the dept
+        name = dep.get_name()
+        connections = dep.get_connections()
 
-        # Add each connection
+        # Add each connection to its corresponding position
         for key in connections:
             # From name -> key
             dist_matrix[name][key] = connections[key]
@@ -35,31 +35,32 @@ def create_dist_matrix(model: Dict[int, Hub]) -> List[List[float]]:
     return dist_matrix
 
 
-def create_pher_matrix(model: Dict[int, Hub], dist_matrix: List[List[float]], p_min=0, p_max=1) -> List[List[float]]:
+def create_pher_matrix(model: Dict[int, Depot], dist_matrix: List[List[float]], p_min=0, p_max=1) -> List[List[float]]:
     """
     Params:
-        model - The hubs in the model as a {name: Hub} dictionary
-        dist_matrix - the corresponding distance matrix showing distances between nodes
+        model - The depots in the model as a {name: DepotObj} dictionary
+        dist_matrix - the corresponding distance matrix showing distances between depots
         p_min - the minimum level of pheromone created - defaults to 0
         p_max - the maximum level of pheromone created - defaults to 1
 
     Returns:
-        Pheremone matrix, with positions corresponding to dist_matrix
+        Pheremone matrix, with positions the same as the dist_matrix
     """
     pher_matrix = []
-    # For each hub in the distance matrix
-    for start_hub in range(len(dist_matrix)):
+    # For each depot in the distance matrix
+    for start_dep in range(len(dist_matrix)):
         row = []
 
-        # If the start_hub is in deficit, add 0 to every pheromone value (as nothing to move)
-        if model[start_hub].get_s() < 0:
+        # If the start_dep is in deficit, add 0 to every pheromone value (as nothing to move)
+        if model[start_dep].get_s() < 0:
             pher_matrix.append([0] * len(dist_matrix))
+            # Continue to the next depot
             continue
 
-        # For each edge from that surplus hub
-        for e in range(len(dist_matrix)):
+        # For each edge from that surplus depot
+        for edge in range(len(dist_matrix)):
             # If is an edge to itself, set pheremone to 0 so never selected
-            if e == start_hub:
+            if edge == start_dep:
                 row.append(0)
                 continue
             # Generate a random number between p_min and p_max and round to 2dp
@@ -71,20 +72,21 @@ def create_pher_matrix(model: Dict[int, Hub], dist_matrix: List[List[float]], p_
     return pher_matrix
 
 
-def create_heur_matrix(dist_matrix: List[List[float]], Q=1) -> List[List[float]]:
+def create_heur_matrix(dist_matrix: List[List[float]]) -> List[List[float]]:
     """
     Creates the heuristic matrix
 
     Params:
         dist_matrix - the corresponding distance matrix
-        Q - the scaling factor for Q/distance, default = 1
 
     returns:
-        Corresponding heuristic matrix to distance matrix
+        A heuristic matrix containing inverse values to the distance matrix
     """
-    # Get the size of the disrance matrix
+    # Get the size of the distance matrix
     size = len(dist_matrix)
-    # Create the heuristic matrix using Q/distance for each edge
-    heur_matrix = [[round(Q/dist_matrix[i][j], 4) if i != j else 0
+
+    # Create the heuristic matrix using 1/distance for each edge
+    heur_matrix = [[round(1/dist_matrix[i][j], 4) if i != j else 0
                     for j in range(size)] for i in range(size)]
+
     return heur_matrix
---
+++ src/tests/searches/aco/test_AS_matrices.py
@@ -0,0 +1,111 @@
+"""
+Tests the matrix creation functions of AS
+"""
+import unittest
+from model.depot import Depot
+from searches.aco.create_matrices import create_dist_matrix, create_pher_matrix, create_heur_matrix
+
+
+class TestASMatricesClass(unittest.TestCase):
+    """
+    Class for testing AS matrix creation
+    """
+    model = {}
+
+    @classmethod
+    def setUpClass(self):
+        """Setup for the tests (to create the model)"""
+        # Create 3 depots
+        dep0 = Depot(name=0, s=-5, long=0, lat=0)
+        dep1 = Depot(name=1, s=6, long=3, lat=4)
+        dep2 = Depot(name=2, s=10, long=5, lat=12)
+
+        # Place in an array
+        self.model = {0: dep0, 1: dep1, 2: dep2}
+
+        # Add the connections
+        dep0.add_connection(dep1)
+        dep0.add_connection(dep2)
+        dep1.add_connection(dep2)
+
+    def test_distance_matrix_creation(self):
+        """
+        Tests that the distance matrix is created correctly
+        """
+        # Obtain the distance matrix
+        dist_matrix = create_dist_matrix(self.model)
+
+        # The expected dist matrix
+        expected_matrix = [
+            [0, 5, 13],
+            [5, 0, 8.246],
+            [13, 8.246, 0]
+        ]
+
+        # Make sure each value in the distance matrix is within 3 decimal places of expected matrix
+        for row in range(len(dist_matrix)):
+            for col in range(len(dist_matrix[row])):
+                self.assertAlmostEqual(
+                    dist_matrix[row][col], expected_matrix[row][col], 3)
+
+    def test_pheromone_matrix_creation(self):
+        """
+        Tests that the pheromone matrix is created correctly
+        """
+        # The dist matrix to be used
+        dist_matrix = [
+            [0, 3, 2],
+            [3, 0, 3],
+            [2, 3, 0]
+        ]
+
+        # Create a pheromone matrix
+        pher_matrix = create_pher_matrix(
+            model=self.model, dist_matrix=dist_matrix)
+
+        # Make sure correct number of rows in pheromone matrix
+        self.assertEqual(len(dist_matrix), len(pher_matrix))
+
+        # for each row
+        for i in range(0, len(dist_matrix)):
+            # Corresponding pheromone matrix row is of correct size
+            self.assertEqual(len(pher_matrix[i]), len(dist_matrix))
+
+            # If the depot is a deficit depot, make sure whole row is 0
+            if self.model[i].get_s() < 0:
+                self.assertTrue((pher_matrix[i] == [0] * len(dist_matrix)))
+                continue
+
+            # For each element in the row of a surplus depot
+            for j in range(0, len(dist_matrix)):
+                # If i and j are same (pheromone for journey from a depot to itself) pheromone is 0
+                if i == j:
+                    self.assertEqual(pher_matrix[i][j], 0)
+                else:
+                    # Make sure pheromone value is between 0 and 1
+                    self.assertGreater(pher_matrix[i][j], 0)
+                    self.assertLessEqual(pher_matrix[i][j], 1)
+
+    def test_heuristic_matrix_creation(self):
+        """
+        Tests that the heuristic matrix is created correctly
+        """
+        # Create a distance matrix
+        dist_matrix = [
+            [0, 4, 2],
+            [4, 0, 4],
+            [2, 4, 0]
+        ]
+
+        # Calculate heuristic matrix
+        h_matrix = create_heur_matrix(dist_matrix=dist_matrix)
+
+        # Make sure 1/each val is returned
+        expected = [
+            [0, 0.25, 0.5],
+            [0.25, 0, 0.25],
+            [0.5, 0.25, 0]
+        ]
+
+        # Assert equal
+        self.assertEqual(h_matrix, expected)
